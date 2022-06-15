9244ea1ed9ada3989832b024f519c8c231a29673
Alex Sielicki <alex@techmilo.com>
2022-06-15 17:16:42 -0600

    Adding more packages to renames

--- src/zest/zodbupdate/renames.py
+++ src/zest/zodbupdate/renames.py
@@ -4,26 +4,72 @@ rename_dict = {
     "App.interfaces IPersistentExtra": iface,
     "App.interfaces IUndoSupport": iface,
     "Products.ResourceRegistries.interfaces.settings IResourceRegistriesSettings": iface,
+    "Products.ATContentTypes.tool.metadata MetadataTool": iface,
+    "Products.ATContentTypes.tool.metadata MetadataSchema": iface,
+    "Products.ATContentTypes.tool.metadata ElementSpec": iface,
+
     "Products.PloneFormGen.content.form FormFolder": iface,
     "Products.PloneFormGen.content.fields FGStringField": iface,
-#    "Products.PloneFormGen.validators.MaxLengthValidator MaxLengthValidator": iface,
+
+    "Products.PloneFormGen.validators.MaxLengthValidator MaxLengthValidator": iface,
+
     "Products.PloneFormGen.content.fields FGTextField": iface,
     "Products.PloneFormGen.content.fields PlainTextField": iface,
+
     "Products.PloneFormGen.content.fields FGDateField": iface,
     "Products.PloneFormGen.content.fields FGIntegerField": iface,
+
     "Products.PloneFormGen.content.fields FGLabelField": iface,
     "Products.PloneFormGen.content.fields FGRichTextField": iface,
+
     "Products.PloneFormGen.content.fields FGSelectionField": iface,
     "Products.PloneFormGen.content.fields FGMultiSelectField": iface,
+
     "Products.PloneFormGen.content.fields FGFieldsetStart": iface,
     "Products.PloneFormGen.content.fields FGFieldsetEnd": iface,
+
     "Products.PloneFormGen.content.fields FGBooleanField": iface,
     "Products.PloneFormGen.content.fields FGRichLabelField": iface,
+
     "Products.PloneFormGen.content.fields FGFileField": iface,
     "Products.PloneFormGen.content.fields HtmlTextField": iface,
+
     "Products.PloneFormGen.content.fields FGLinesField": iface,
     "Products.PloneFormGen.content.fields NRBooleanField": iface,
 
+    "Products.Archetypes.BaseUnit BaseUnit": iface,
+
+    "Products.PloneFormGen.content.formMailerAdapter FormMailerAdapter": iface,
+
+    "Products.PloneFormGen.content.thanksPage FormThanksPage": iface,
+
+    "Products.PloneFormGen.content.fieldset FieldsetFolder": iface,
+
+    "Products.PloneFormGen.content.saveDataAdapter FormSaveDataAdapter": iface,
+    "Products.PloneFormGen.interfaces.form IPloneFormGenForm": iface,
+
+    "Products.ATContentTypes.interfaces.folder IATBTreeFolder": iface,
+    "Products.ATContentTypes.interfaces.folder IATFolder": iface,
+
+    "Products.ATContentTypes.tool.metadata MetadataElementPolicy": iface,
+    "Products.PloneFormGen.content.formLikertField FGLikertField": iface,
+    "Products.PloneFormGen.content.customScriptAdapter FormCustomScriptAdapter": iface,
+
+    "Products.ATContentTypes.interfaces.interfaces IATContentType": iface,
+
+    "Products.Archetypes.interfaces.referenceable IReferenceable": iface,
+    "plone.app.folder.folder IATUnifiedFolder": iface,
+    "archetypes.multilingual.interfaces IArchetypesTranslatable": iface,
+
+    "Products.Archetypes.interfaces.base IBaseContent": iface,
+    "Products.Archetypes.interfaces.base IBaseFolder": iface,
+    "Products.Archetypes.interfaces.base IBaseObject": iface,
+
+    "plone.app.imaging.interfaces IBaseObject": iface,
+
+    "archetypes.schemaextender.interfaces IExtensible": iface,
+    "Products.Archetypes.interfaces.metadata IExtensibleMetadata": iface,
+    "Products.PloneFormGen.interfaces.thanksPage IPloneFormGenThanksPage": iface,
 
 }
 # I have seen zodbverify on Python 2 complain with a warning about various webdav.interfaces factories.
